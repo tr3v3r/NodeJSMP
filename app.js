7eacd78947d3e2b3cb65d1107a82938112f33a67
Aliaksei Astafyeu <aliaksei_astafyeu@epbyminw7097.minsk.epam.com>
2018-04-23 10:31:56 +0300

    fix minor mistakes after review

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import config from './config/config.json';
-import { User, Product } from './modules';
+import config from './config/config';
+import { User, Product } from './models';
 
-console.log(config.name)
+console.log(config.name);
 const user = new User();
 const product = new Product();
