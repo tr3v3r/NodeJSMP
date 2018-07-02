0761c93473ed50293a3b8f48d1820b4631885aaa
Alexey <lochost40@gmail.com>
2018-07-02 19:09:20 +0300

    fix review issues

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,21 +1,21 @@
 import jwt from 'jsonwebtoken';
 import { find } from 'lodash';
 import { usersData } from '../../models';
-
+import { tokenExpiresIn } from './constants';
 
 const authController = {
   authPost(req, res) {
     const user = find(usersData, { name: req.body.username });
     if (user === undefined || user.password !== req.body.password) {
-      res.status(403).send({
-        code: 403,
+      res.status(404).send({
+        code: 404,
         message: 'Not found',
         data: { },
       });
     } else {
       const { id, isModerator, email, name } = user;
       const payload = { sub: id, isModerator };
-      const token = jwt.sign(payload, 'secret', { expiresIn: 100 });
+      const token = jwt.sign(payload, 'secret', { expiresIn: tokenExpiresIn });
       const response = {
         code: 200,
         message: 'OK',
@@ -32,20 +32,38 @@ const authController = {
   },
 
   localPassport(req, res) {
-    res.json(req.user);
+    if (!req.user) {
+      res.json({ error: 'User is not found' });
+    } else {
+      res.json(req.user);
+    }
   },
 
   facebookPassport(req, res) {
-    res.json(req.user);
+    if (!req.user) {
+      res.json({ error: 'User is not found' });
+    } else {
+      res.json(req.user);
+    }
   },
 
   twitterPassport(req, res) {
-    res.json(req.user);
+    if (!req.user) {
+      res.json({ error: 'User is not found' });
+    } else {
+      res.json(req.user);
+    }
   },
 
   googlePassport(req, res) {
-    res.json(req.user);
+    if (!req.user) {
+      res.json({ error: 'User is not found' });
+    } else {
+      res.json(req.user);
+    }
   }
+
+
 };
 
 
